751f0f9daad288838435514cc0ee14ea7693d715
Georgii Gromov <43499418+giothun@users.noreply.github.com>
2021-10-22 19:41:56 +0300

    Delete Config.java

--- app/src/main/java/com/example/Classes/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.Classes;
-
-public class Config {
-    public static final String EMAIL ="goshakrutoy123@gmail.com";
-    public static final String PASSWORD ="JeN-P68-Nvw-cP4";
-}
